7285a7985d32ce6d657eb463bfcb8d807ab06a10
GallantDrop45 <tiago.xavier45@outlook.com>
2026-02-14 19:07:22 -0300

    CalculadoraV1

---
+++ Calculadora/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine($"Escolha a operação desejada + - / * %");
+string operacao = Console.ReadLine();
+
+Console.WriteLine("Escolha primiero Número");
+Double PNum = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Escolha segundo Número");
+Double SNum = Convert.ToDouble(Console.ReadLine());
+
+if (operacao == "+")
+{
+    double resultado = PNum + SNum;
+    Console.WriteLine(resultado);
+}
+else if (operacao == "-")
+{
+    double resultado = PNum- SNum;
+    Console.WriteLine(resultado);
+}
+else if (operacao == "/")
+{
+    double resultado = PNum / SNum;
+    Console.WriteLine(resultado);
+}
+else if (operacao == "*")
+{
+    double resultado = PNum * SNum;
+    Console.WriteLine(resultado);
+}
+else if (operacao == "%")
+{
+    double resultado = PNum % SNum;
+    Console.WriteLine(resultado);
+}
+
+Console.WriteLine("Operação finalizada");
